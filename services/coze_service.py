49b82800fd5354165040912d8ffc4c13314da67b
liangbingyihao <liangbingyihao@163.com>
2025-04-18 17:49:18 +0800

    ~

--- services/coze_service.py
+++ services/coze_service.py
@@ -1,23 +1,16 @@
 import logging
 import os
+import time
 from concurrent.futures import ThreadPoolExecutor
+from extensions import db
 
 coze_api_token = os.getenv("COZE_API_TOKEN")
 
-from cozepy import Coze, TokenAuth, Message, ChatStatus, MessageContentType, ChatEventType, COZE_CN_BASE_URL, \
-    COZE_COM_BASE_URL  # noqa
+from cozepy import Coze, TokenAuth, Message, ChatEventType, COZE_CN_BASE_URL,COZE_COM_BASE_URL  # noqa
 
 # Init the Coze client through the access_token.
 coze = Coze(auth=TokenAuth(token=coze_api_token), base_url=COZE_CN_BASE_URL)
 
-
-# Create a bot instance in Coze, copy the last number from the web link as the bot's ID.
-# The user id identifies the identity of a user. Developers can use a custom business ID
-# or a random string.
-
-# Call the coze.chat.stream method to create a chat. The create method is a streaming
-# chat and will return a Chat Iterator. Developers should iterate the iterator to get
-# chat event and handle them.
 
 class CozeService:
     bot_id = "7481241756508504091"
@@ -27,13 +20,15 @@ class CozeService:
     def chat_with_coze_async(user_id, context_id):
         try:
             logging.warning(f"chat_with_coze_async: {user_id, context_id}")
-            CozeService.executor.submit(CozeService.chat_with_coze, (user_id, context_id))
+            CozeService.executor.submit(CozeService.chat_with_coze, user_id, context_id)
         except Exception as e:
             logging.exception(e)
 
     @staticmethod
     def chat_with_coze(user_id, context_id):
+        session = None
         try:
+            session = db.create_scoped_session()
             from models.message import Message
             from models.user import User
             message = Message.query.filter_by(id=context_id, status=0).with_entities(Message.content).one()
@@ -41,6 +36,9 @@ class CozeService:
             logging.warning(f"GOT: {user_id, context_id, message, user}")
         except Exception as e:
             logging.exception(e)
+        finally:
+            if session:
+                session.remove()  # 重要！清理会话
 
     @staticmethod
     def _chat_with_coze(cls, user_id, msg):
