c3701f6de61908c8605fcd3901c0b65c78a8ec19
liangbingyihao <liangbingyihao@163.com>
2025-04-21 09:17:44 +0800

    ~

--- services/coze_service.py
+++ services/coze_service.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import sessionmaker,scoped_session
 from concurrent.futures import ThreadPoolExecutor
 
@@ -17,7 +17,7 @@ engine = create_engine(
     max_overflow=0,  # 超过连接池大小外最多创建的连接
     pool_size=5,  # 连接池大小
     pool_timeout=30,  # 池中没有线程最多等待的时间，否则报错
-    pool_recycle=-1  # 多久之后对线程池中的线程进行一次连接的回收（重置）
+    pool_recycle=3600  # 多久之后对线程池中的线程进行一次连接的回收（重置）
 )
 # 第二步：拿到一个Session类,传入engine
 Session = sessionmaker(bind=engine)
@@ -38,12 +38,24 @@ class CozeService:
     @staticmethod
     def chat_with_coze(user_id, context_id):
         session = None
+        from models.user import User
         try:
             session = Session()
-            from models.message import Message
-            from models.user import User
-            message = session.query(Message).filter_by(id=context_id, status=0).with_entities(Message.content,Message.session_id).one()
             user =  session.query(User).filter_by(id=user_id).with_entities(User.username).one()
+        except exc.OperationalError as e:
+            session.rollback()
+            logging.exception(e)
+            engine.dispose()
+            session = Session()
+            user =  session.query(User).filter_by(id=user_id).with_entities(User.username).one()
+        except Exception as e:
+            logging.exception(e)
+            return
+
+
+        try:
+            from models.message import Message
+            message = session.query(Message).filter_by(id=context_id, status=0).with_entities(Message.content,Message.session_id).one()
             logging.warning(f"start: {user_id, context_id, message, user}")
             response = CozeService._chat_with_coze(user[0],message[0])
             if response:
