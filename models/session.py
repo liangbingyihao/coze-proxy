0fe304c971e17f5913b6e126e884fdc83a3733ee
liangbingyihao <liangbingyihao@163.com>
2025-05-15 17:56:03 +0800

    ~

--- models/session.py
+++ models/session.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import time
+
 from extensions import db
 
 
@@ -11,8 +12,19 @@ class Session(db.Model):
     robt_id = db.Column(db.Integer, index=True, nullable=False)
     tags = db.Column(db.String(255))
     conversation_id = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    # 创建时间（自动设置）
+    created_at = db.Column(db.Integer, default=lambda: int(time.time()))
+    updated_at = db.Column(db.Integer, default=lambda: int(time.time()))
+
+    # # 更新时间（自动更新）
+    # updated_at = db.Column(
+    #     TIMESTAMP,
+    #     server_default=text('CURRENT_TIMESTAMP'),
+    #     server_onupdate=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+    #     nullable=False
+    # )
+    # created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    # updated_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
     def __init__(self, session_name, owner_id, robt_id):
         self.session_name = session_name or ""
