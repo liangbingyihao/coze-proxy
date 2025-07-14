feecf319b72b37700265020d8da1dc36184ca24f
liangbingyihao <liangbingyihao@163.com>
2025-07-14 18:57:31 +0800

    me.publicid

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,7 +1,7 @@
 from marshmallow import Schema, fields
 
 class UserSchema(Schema):
-    id = fields.Str(dump_only=True,data_key="public_id")
+    public_id = fields.Str(dump_only=True,data_key="id")
     username = fields.Str(required=True)
     email = fields.Email(required=True)
     created_at = fields.DateTime(dump_only=True)
